597113e3cf6212b519614c1d1d03269bab4e039a
Mostafa <yousrey.mostafa@gmail.com>
2026-09-15 08:38:11 +0300

    Refresh PWA cache for v15

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='my-duaa-v14';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./app-icon.png'];
+const CACHE='my-duaa-v15';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./logo.svg','./app-icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const c=res.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return res}))) });
