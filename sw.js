6fc84cd10a4001022f46358b52de4a780710e494
Mostafa <yousrey.mostafa@gmail.com>
2026-09-18 16:52:36 +0300

    Refresh clean release cache r2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='my-duaa-clean-r1';
+const CACHE='my-duaa-clean-r2';
 const CORE=['./','./index.html','./styles.css','./app.js','./manifest.json','./logo.svg','./app-icon.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
